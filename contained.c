634accaf2c52a81ac61e2d693bb00f6d0f664d8e
aln730 <arnavgawas707@gmail.com>
2025-11-23 02:47:45 -0500

    implemented namespaces

--- contained.c
+++ contained.c
@@ -46,7 +46,34 @@ struct child_config {
 
 <<child>>
 
-<<choose-hostname>>
+int choose_hostname(char *buff, size_t len)
+{
+	static const char *suits[] = { "swords", "wands", "pentacles", "cups" };
+	static const char *minor[] = {
+		"ace", "two", "three", "four", "five", "six", "seven", "eight",
+		"nine", "ten", "page", "knight", "queen", "king"
+	};
+	static const char *major[] = {
+		"fool", "magician", "high-priestess", "empress", "emperor",
+		"hierophant", "lovers", "chariot", "strength", "hermit",
+		"wheel", "justice", "hanged-man", "death", "temperance",
+		"devil", "tower", "star", "moon", "sun", "judgment", "world"
+	};
+	struct timespec now = {0};
+	clock_gettime(CLOCK_MONOTONIC, &now);
+	size_t ix = now.tv_nsec % 78;
+	if (ix < sizeof(major) / sizeof(*major)) {
+		snprintf(buff, len, "%05lx-%s", now.tv_sec, major[ix]);
+	} else {
+		ix -= sizeof(major) / sizeof(*major);
+		snprintf(buff, len,
+			 "%05lxc-%s-of-%s",
+			 now.tv_sec,
+			 minor[ix % (sizeof(minor) / sizeof(*minor))],
+			 suits[ix / (sizeof(minor) / sizeof(*minor))]);
+	}
+	return 0;
+}
 
 int main (int argc, char **argv)
 {
@@ -107,7 +134,44 @@ finish_options:
 		goto error;
 	config.hostname = hostname;
 
-<<namespaces>>
+	if (socketpair(AF_LOCAL, SOCK_SEQPACKET, 0, sockets)) {
+		fprintf(stderr, "socketpair failed: %m\n");
+		goto error;
+	}
+	if (fcntl(sockets[0], F_SETFD, FD_CLOEXEC)) {
+		fprintf(stderr, "fcntl failed: %m\n");
+		goto error;
+	}
+	config.fd = sockets[1];
+
+	#define STACK_SIZE (1024 * 1024)
+
+	char *stack = 0;
+	if (!(stack = malloc(STACK_SIZE))) {
+		fprintf(stderr, "=> malloc failed, out of memory?\n");
+		goto error;
+	}
+
+	if (resources(&config)) {
+		err = 1;
+		goto clear_resources;
+	}
+
+	int flags = CLONE_NEWNS
+		| CLONE_NEWCGROUP
+		| CLONE_NEWPID
+		| CLONE_NEWIPC
+		| CLONE_NEWNET
+		| CLONE_NEWUTS;
+
+	if ((child_pid = clone(child, stack + STACK_SIZE, flags | SIGCHILD, &config)) == -1) {
+		fprintf(stderr, "=> clone failed! %m\n");
+		err = 1;
+		goto clear_resources;
+	}
+
+	close(sockets[1]);
+	sockets[1] = 0;
 
 	goto cleanup;
 usage:
